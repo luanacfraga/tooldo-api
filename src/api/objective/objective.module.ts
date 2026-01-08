cb4ff5dc69adbb040ed615e9d5d3e59cca0ac006
Luana <luana.cam20@gmail.com>
2026-01-08 00:35:35 -0300

    feat(objective): add Objective module with CRUD operations and integrate into app module
    - Introduced ObjectiveModule, including ObjectiveController for managing objectives with endpoints for listing, creating, updating, and deleting objectives.
    - Implemented role-based access control for endpoints, allowing only ADMIN and MANAGER roles to perform actions.
    - Updated app.module.ts to include ObjectiveModule and modified Prisma schema to add objectives table with necessary relationships.
    - Created migration script for objectives table and added relevant DTOs for structured data handling.
    
    These changes enhance the application's functionality by enabling objective management, improving team and company goal tracking.

---
+++ src/api/objective/objective.module.ts
@@ -0,0 +1,11 @@
+import { DatabaseModule } from '@/infra/database/database.module';
+import { Module } from '@nestjs/common';
+import { ObjectiveController } from './objective.controller';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [ObjectiveController],
+})
+export class ObjectiveModule {}
+
+
